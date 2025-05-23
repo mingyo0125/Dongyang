c97f6cdf41399ac31d11886a3cce8fd84163d716
mingyo0125 <98925706+mingyo0125@users.noreply.github.com>
2025-05-23 10:04:59 +0900

    2025_05_14_py

--- Java/Study.java
+++ Java/Study.java
@@ -3,14 +3,20 @@ public class Study
 
     public static void main(String[] args)
     {
-        int i = 20;
-        int s = 0;
-        char a = ' ';
+        int[] arr = {0,1,2,3,4,5};
+        int i = 0;
+
+        try {
+            for(i = 0; i < 7; i++)
+            {
+                System.err.println(arr[i]);
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+            System.err.print(i);
+            System.err.print("번째 인덱스가 배열의 범위를 벗어났습니다.");
+        }
+
         
-        s = 10;
-        a = '?';
-        System.out.println(a);
-        System.out.println("Hello");
-        System.out.println(s);
     }
 }
